5eca078f295220e0d0ff615c19ddcf4d004491dc
Maurizio <proiettomaurizio97@gmail.com>
2021-12-13 17:24:24 +0100

    macrostructure footer

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,6 +1,16 @@
 <template>
-  <footer>
+  <footer class="bg-green">
+    <!-- top footer -->
+    <div id="top-footer">
 
+    </div>
+    <!-- /top footer -->
+
+    <!-- bottom footer -->
+    <div id="bottom-footer">
+
+    </div>
+    <!-- /bottom footer -->
   </footer>
 </template>
 
@@ -12,5 +22,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import '@/assets/_variables.scss';
 
+footer {
+  height: 500px;
+}
 </style>
