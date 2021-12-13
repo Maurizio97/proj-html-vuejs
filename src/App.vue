c1bef2ef86ed4c22e487cff818d35a3fa72bee7d
Maurizio <proiettomaurizio97@gmail.com>
2021-12-13 12:35:51 +0100

    add: fontawesome and color variable with sass

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,9 @@ export default {
 </script>
 
 <style lang="scss">
+$blueBanner: #5babd9;
+$green: #6abf17;
+$colorButton: #1775bf;
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -30,4 +33,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,16 +1,23 @@
 <template>
   <header>
-
+    <span>
+        ciao sono mario
+      <i class="fas fa-phone"></i>
+    </span>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'MyHeader',
-}
+  name: "MyHeader",
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+$green: #6abf17;
+span {
+  color: $green;
+  font-size: 100px;
+}
 </style>
