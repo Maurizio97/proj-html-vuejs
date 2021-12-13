5b77458edfc2d9ad830ca3a6689297e8a40ca809
Maurizio <proiettomaurizio97@gmail.com>
2021-12-13 12:40:18 +0100

    macrostructure header

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,9 +1,24 @@
 <template>
   <header>
-    <span>
-        ciao sono mario
-      <i class="fas fa-phone"></i>
-    </span>
+    <!-- <i class="fas fa-phone"></i> -->
+    <!-- banner Contacts -->
+    <div>
+
+    </div>
+
+    <!-- jumbotron  -->
+    <div>
+      <!-- navbar -->
+      <nav>
+
+      </nav>
+      <!-- /navbar -->
+    </div>
+
+    <!-- Banner Get Quote  -->
+    <div>
+
+    </div>
   </header>
 </template>
 
@@ -16,8 +31,5 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 $green: #6abf17;
-span {
-  color: $green;
-  font-size: 100px;
-}
+
 </style>
