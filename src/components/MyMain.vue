fe05bdafd6f38057f68ab165fdc678fcdf35f2c9
Maurizio <proiettomaurizio97@gmail.com>
2021-12-13 11:32:13 +0100

    create macrostructure components

---
+++ src/components/MyMain.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'MyMain',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
