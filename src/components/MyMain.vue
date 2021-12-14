3b127358e6a878361e147fbf8cf9ad7b3e115397
Maurizio <proiettomaurizio97@gmail.com>
2021-12-14 11:20:45 +0100

    create new components MySection2

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,17 +1,20 @@
 <template>
   <main>
     <MySection1/>
+    <MySection2/>
     <MySection5/>
   </main>
 </template>
 
 <script>
 import MySection1 from '../components/MySection1.vue'
+import MySection2 from '../components/MySection2.vue'
 import MySection5 from '../components/MySection5.vue'
 export default {
   name: 'MyMain',
   components: {
     MySection1,
+    MySection2,
     MySection5,
   },
   
---
+++ src/components/MySection2.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+      <div>
+          <h3 class="sub-title">
+          </h3>
+          <h2 class="title">
+          </h2>
+          <p class="text-par">
+          </p>
+          <button></button>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'MySection2',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+div {
+    text-align: center;
+    margin: 150px 0;
+
+    h2 {
+        width: 25%;
+    }
+
+    p {
+        width: 40%;
+        margin: 30px auto;
+    }
+}
+</style>
